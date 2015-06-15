4558ad03dceb5965017e175adaf9ca246edc0f6a
umesh kumar <umeshk820@gmail.com>
2015-06-15 23:14:25 +0530

    / base url mapping done

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,8 +6,8 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-
-        "/"(view:"/index")
+         "/"(controller:"home",action: "index");
+        //"/"(view:"/index")
         "500"(view:'/error')
 	}
 }
