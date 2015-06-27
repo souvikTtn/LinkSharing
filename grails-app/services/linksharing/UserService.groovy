e7ad4c0217ea057d825ce03cbf9e53e870458617
umesh kumar <umeshk820@gmail.com>
2015-06-28 04:23:42 +0530

    user service created

---
+++ grails-app/services/linksharing/UserService.groovy
@@ -0,0 +1,51 @@
+package linksharing
+import linksharing.*
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.springframework.web.multipart.MultipartFile
+import grails.transaction.Transactional
+
+@Transactional
+class UserService {
+
+    def serviceMethod() {
+
+    }
+
+    User currentUser(String uid){
+
+        return User.get(uid)
+
+    }
+
+    def String uploadFile(MultipartFile file,
+                          String name, String destinationDirectory) {
+        def servletContext = ServletContextHolder.servletContext
+        def storagePath = servletContext.getRealPath(destinationDirectory)
+// Create storage path directory if it does not exist
+        def storagePathDirectory = new File(storagePath)
+        if (!storagePathDirectory.exists()) {
+            print "CREATING DIRECTORY ${storagePath}: "
+            if (storagePathDirectory.mkdirs()) {
+                println "SUCCESS"
+            } else {
+                println "FAILED"
+            }
+        }
+// Store file
+        if (!file.isEmpty()) {
+            file.transferTo(new File("${storagePath}/${name}"))
+            println "Saved file: ${storagePath}/${name}"
+            return "${storagePath}/${name}"
+        } else {
+            println "File ${file.inspect()} was empty!"
+            return null
+        }
+    }
+
+
+
+
+
+
+
+}
